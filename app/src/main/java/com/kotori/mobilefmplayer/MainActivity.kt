ad1922fffe99e1084b5b1ed0f5d610ea820d600a
Minami Kotori <15900197156@163.com>
2022-01-08 21:12:36 +0800

    测试QMUI

--- app/src/main/java/com/kotori/mobilefmplayer/MainActivity.kt
+++ app/src/main/java/com/kotori/mobilefmplayer/MainActivity.kt
@@ -1,15 +1,17 @@
 package com.kotori.mobilefmplayer
 
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.kotori.common.sdk.testSDKGetCategories
 import com.qmuiteam.qmui.arch.QMUIActivity
 import com.qmuiteam.qmui.widget.QMUIWindowInsetLayout2
 
-class MainActivity : QMUIActivity() {
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        actionBar?.hide()
 
         testSDKGetCategories()
     }
