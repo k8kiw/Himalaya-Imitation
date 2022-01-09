c88cc8051ca7c9cc8f323d89258762ecc2c02771
Minami Kotori <15900197156@163.com>
2022-01-09 14:12:23 +0800

    测试QMUI的actionbar问题

--- app/src/main/java/com/kotori/mobilefmplayer/MainActivity.kt
+++ app/src/main/java/com/kotori/mobilefmplayer/MainActivity.kt
@@ -1,18 +1,31 @@
 package com.kotori.mobilefmplayer
 
+import android.app.Activity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import com.kotori.common.sdk.testSDKGetCategories
+import com.kotori.mobilefmplayer.databinding.ActivityMainBinding
 import com.qmuiteam.qmui.arch.QMUIActivity
-import com.qmuiteam.qmui.widget.QMUIWindowInsetLayout2
+import com.qmuiteam.qmui.arch.QMUIFragmentActivity
+import com.qmuiteam.qmui.util.QMUIStatusBarHelper
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : QMUIActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val mBinding = DataBindingUtil.inflate<ActivityMainBinding>(
+            layoutInflater,
+            R.layout.activity_main,
+            null,
+            false
+        )
+        setContentView(mBinding.root)
+
         actionBar?.hide()
 
-        testSDKGetCategories()
+        mBinding.topBar.setTitle("Title")
+
+        //testSDKGetCategories()
     }
 }
