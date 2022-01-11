d1a0064e5853a8312ad9e7acb753d77890c290b0
Minami Kotori <15900197156@163.com>
2022-01-11 13:17:45 +0800

    更新常量

---
+++ common/src/main/java/com/kotori/common/support/Constants.kt
@@ -0,0 +1,14 @@
+package com.kotori.common.support
+
+
+/**
+ * 保存全局使用的常量
+ * 各个模块跳转、通信，所需要用到的路径信息
+ */
+object Constants {
+
+    const val PATH_MAIN = "/app/MainActivity"
+
+    // TODO:默认的返回键
+    const val DEFAULT_LEFT_IMAGE: Int = 1
+}
