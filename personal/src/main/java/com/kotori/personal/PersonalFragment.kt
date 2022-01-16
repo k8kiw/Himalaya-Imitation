45e9d88fb439ceca208025d1ac15765b093e7bad
Minami Kotori <15900197156@163.com>
2022-01-16 20:39:05 +0800

    personal模块初始化界面

---
+++ personal/src/main/java/com/kotori/personal/PersonalFragment.kt
@@ -0,0 +1,34 @@
+package com.kotori.personal
+
+import android.view.View
+import com.kotori.common.base.BaseDbFragment
+import com.kotori.common.entity.ProgressBean
+import com.kotori.common.ui.addLeftCloseImageBtn
+import com.kotori.personal.databinding.FragmentPersonalBinding
+
+class PersonalFragment: BaseDbFragment<FragmentPersonalBinding>() {
+    override fun onLazyInit() {
+        TODO("Not yet implemented")
+    }
+
+    override fun getLayoutId(): Int = R.layout.fragment_personal
+
+    override fun showTopBar(): Boolean = true
+
+
+    override fun initView(root: View) {
+        getTopBar()?.setTitle(R.string.title_personal)
+
+        // 测试返回键
+        addLeftCloseImageBtn()
+    }
+
+    override fun showProgress(progress: ProgressBean) {
+        TODO("Not yet implemented")
+    }
+
+    override fun hideProgress() {
+        TODO("Not yet implemented")
+    }
+
+}
