0c71c464dc1026f00d0dd2fd9cc74cdcef27f407
Minami Kotori <15900197156@163.com>
2022-01-20 16:02:14 +0800

    添加搜索键，测试协程封装后的SDK内容获取

--- home/src/main/java/com/kotori/home/HomeFragment.kt
+++ home/src/main/java/com/kotori/home/HomeFragment.kt
@@ -1,12 +1,21 @@
 package com.kotori.home
 
 import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import com.alibaba.android.arouter.facade.annotation.Route
+import com.alibaba.android.arouter.launcher.ARouter
 import com.kotori.common.base.BaseDbFragment
 import com.kotori.common.entity.ProgressBean
+import com.kotori.common.sdk.SDKCallbackExt
+import com.kotori.common.support.Constants
 import com.kotori.common.ui.showInfoTipsDialog
 import com.kotori.common.utils.LogUtil
+import com.kotori.common.utils.showToast
 import com.kotori.home.databinding.FragmentHomeBinding
+import kotlinx.coroutines.launch
 
+@Route(path = Constants.PATH_HOME_PAGE)
 class HomeFragment : BaseDbFragment<FragmentHomeBinding>() {
     override fun onLazyInit() {
         TODO("Not yet implemented")
@@ -19,14 +28,38 @@ class HomeFragment : BaseDbFragment<FragmentHomeBinding>() {
 
     override fun initView(root: View) {
         getTopBar()?.setTitle(R.string.title_home)
+        getTopBar()?.addRightImageButton(
+            R.drawable.ic_search_24px_rounded,
+            R.id.topbar_right_about_button
+        )?.setOnClickListener { view ->
+            "搜索被点击".showToast()
+        }
 
         LogUtil.d("TTTTTTTTTTTTTT------>", "$parentFragment")
         LogUtil.d("TTTTTTTTTTTTTT------>", "${showTopBar()}")
 
         mBinding.homeTestButton.setOnClickListener {
-            showInfoTipsDialog("点击了按钮")
+
+            // 请求网络数据，记得捕获没网异常
+            lifecycleScope.launch {
+                val categories = SDKCallbackExt.getAllCategories(null)
+                categories?.let { list ->
+                    LogUtil.d(TAG, list.size.toString())
+
+                    list.forEach {
+                        LogUtil.d(TAG, it.toString())
+                    }
+                }
+            }
 
         }
+    }
+
+
+    private fun startPlayerPage() {
+        ARouter.getInstance().build(Constants.PATH_PLAYER_PAGE)
+            .withString("test", "路由跳转")
+            .navigation()
     }
 
 
