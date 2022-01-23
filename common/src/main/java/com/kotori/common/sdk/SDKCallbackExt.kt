ebd3841d50ae43c859676d2f97b714849a7c3f30
Minami Kotori <15900197156@163.com>
2022-01-23 12:04:24 +0800

    缓存推荐列表，无需每次都请求

--- common/src/main/java/com/kotori/common/sdk/SDKCallbackExt.kt
+++ common/src/main/java/com/kotori/common/sdk/SDKCallbackExt.kt
@@ -41,39 +41,48 @@ object SDKCallbackExt {
 
     /**
      * 获取推荐专辑，将全区的编辑推荐整合为一个大列表
-     * 方便进行分页加载
+     * 请求一次，缓存下来，之后就不需要请求网络了
      * index需要进行限制，不能无限多
      */
+    private val allResultList = ArrayList<Album>()
     suspend fun getRecommendAlbumList(startIndex : Int, endIndex : Int) : List<Album> {
         return suspendCoroutine { continuation ->
-            // 设置参数，每类推荐10个
-            val params = mapOf(
-                DTransferConstants.DISPLAY_COUNT to 10.toString()
-            )
-            // 调用编辑推荐
-            CommonRequest.getDiscoveryRecommendAlbums(params, object : IDataCallBack<DiscoveryRecommendAlbumsList> {
-                override fun onSuccess(p0: DiscoveryRecommendAlbumsList?) {
-                    // 此处的返回是每个分类10个的列表，要将各个分类整个为统一的列表
-                    val allResultList = ArrayList<Album>()
-                    // 拿到结果
-                    val recommendAlbumsList = p0?.discoveryRecommendAlbumses
-                    // 遍历每个分类的列表
-                    recommendAlbumsList?.forEach { recommendAlbums ->
-                        // 每个分类下实际的list
-                        val currentAlbumList = recommendAlbums.albumList
-                        // 将该list插入到结果中
-                        allResultList += currentAlbumList
+            // 若列表里有数据直接返回，不然就请求一次
+            if (allResultList.isNotEmpty()) {
+                // 根据index返回实际的结果
+                val result = allResultList.subList(startIndex, endIndex)
+                // 这个resume不会截断后面的代码，不能resume多次，需要else
+                continuation.resume(result)
+            } else {
+                // 设置参数，每类推荐10个
+                val params = mapOf(
+                    DTransferConstants.DISPLAY_COUNT to 10.toString()
+                )
+                // 调用编辑推荐
+                CommonRequest.getDiscoveryRecommendAlbums(params, object : IDataCallBack<DiscoveryRecommendAlbumsList> {
+                    override fun onSuccess(p0: DiscoveryRecommendAlbumsList?) {
+                        // 此处的返回是每个分类10个的列表，要将各个分类整个为统一的列表
+                        // 拿到结果
+                        val recommendAlbumsList = p0?.discoveryRecommendAlbumses
+                        // 遍历每个分类的列表
+                        recommendAlbumsList?.forEach { recommendAlbums ->
+                            // 每个分类下实际的list
+                            val currentAlbumList = recommendAlbums.albumList
+                            // 将该list插入到结果中
+                            allResultList += currentAlbumList
+                        }
+                        // 根据index返回实际的结果
+                        val result = allResultList.subList(startIndex, endIndex)
+                        continuation.resume(result)
                     }
-                    // 根据index返回实际的结果
-                    val result = allResultList.subList(startIndex, endIndex)
-                    continuation.resume(result)
-                }
 
-                override fun onError(p0: Int, p1: String?) {
-                    continuation.resumeWithException(TimeoutException(p1))
-                }
+                    override fun onError(p0: Int, p1: String?) {
+                        continuation.resumeWithException(TimeoutException(p1))
+                    }
 
-            })
+                })
+            }
+
         }
     }
 
