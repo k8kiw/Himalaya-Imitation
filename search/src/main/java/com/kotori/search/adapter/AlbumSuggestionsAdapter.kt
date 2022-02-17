7ce9c3df1767a46165a9149ddcdfe8c625c8c1bf
Minami Kotori <15900197156@163.com>
2022-02-17 13:12:47 +0800

    完成搜索框联想内容的adapter，使其能够正确搜索

---
+++ search/src/main/java/com/kotori/search/adapter/AlbumSuggestionsAdapter.kt
@@ -0,0 +1,69 @@
+package com.kotori.search.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Filter
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.kotori.search.R
+import com.mancj.materialsearchbar.adapter.SuggestionsAdapter
+import com.ximalaya.ting.android.opensdk.model.word.QueryResult
+
+
+class AlbumSuggestionsAdapter constructor(
+    private val inflater: LayoutInflater
+): SuggestionsAdapter<QueryResult, AlbumSuggestionsAdapter.SuggestionHolder>(inflater) {
+
+    override fun getSingleViewHeight(): Int = 50
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SuggestionHolder {
+        val rootView = inflater.inflate(R.layout.item_suggestions, parent, false)
+        return SuggestionHolder(rootView)
+    }
+
+    override fun onBindSuggestionHolder(
+        suggestion: QueryResult?,
+        holder: SuggestionHolder?,
+        position: Int
+    ) {
+        holder?.title?.text = suggestion?.keyword
+    }
+
+    /**
+     * 过滤器
+     */
+    override fun getFilter(): Filter {
+        return object : Filter() {
+            override fun performFiltering(constraint: CharSequence?): FilterResults {
+                val results = FilterResults()
+                val term = constraint.toString()
+                if (term.isEmpty()) {
+                    suggestions = suggestions_clone
+                } else {
+                    suggestions = ArrayList()
+                    for (item in suggestions_clone) {
+                        if (item.keyword.lowercase().contains(term.lowercase())) {
+                            suggestions.add(item)
+                        }
+                    }
+                }
+                results.values = suggestions
+                return results
+            }
+
+            override fun publishResults(constraint: CharSequence?, results: FilterResults?) {
+                suggestions = results?.values as ArrayList<QueryResult>
+                notifyDataSetChanged()
+            }
+
+        }
+    }
+
+    inner class SuggestionHolder constructor(
+        itemView: View
+    ): RecyclerView.ViewHolder(itemView){
+        val title: TextView = itemView.findViewById(R.id.search_suggestion_title)
+    }
+}
--- search/src/main/java/com/kotori/search/ui/SearchActivity.kt
+++ search/src/main/java/com/kotori/search/ui/SearchActivity.kt
@@ -2,6 +2,7 @@ package com.kotori.search.ui
 
 import android.text.Editable
 import android.text.TextWatcher
+import android.view.LayoutInflater
 import android.view.View
 import android.widget.ImageView
 import com.alibaba.android.arouter.facade.annotation.Route
@@ -10,8 +11,10 @@ import com.kotori.common.entity.ProgressBean
 import com.kotori.common.support.Constants
 import com.kotori.common.utils.showToast
 import com.kotori.search.R
+import com.kotori.search.adapter.AlbumSuggestionsAdapter
 import com.kotori.search.databinding.ActivitySearchBinding
 import com.mancj.materialsearchbar.MaterialSearchBar
+import com.ximalaya.ting.android.opensdk.model.word.QueryResult
 
 @Route(path = Constants.PATH_SEARCH_PAGE)
 class SearchActivity : BaseActivity<ActivitySearchBinding>(){
@@ -30,7 +33,7 @@ class SearchActivity : BaseActivity<ActivitySearchBinding>(){
             // 设置搜索框样式
             setPlaceHolder("搜索你感兴趣的内容吧")
             setHint("输入搜索内容")
-            setMaxSuggestionCount(10)
+            setMaxSuggestionCount(5)
             setSpeechMode(false)
             setRoundedSearchBarEnabled(true)
             // 调整card view 向后的阴影程度
@@ -42,9 +45,16 @@ class SearchActivity : BaseActivity<ActivitySearchBinding>(){
             setNavIcon(R.drawable.ic_home_used_in_search_bar)
 
             // 设置联想内容适配器
-
-            val testList = listOf("111", "222", "444", "666")
-            lastSuggestions = testList
+            val layoutInflater = getSystemService(LAYOUT_INFLATER_SERVICE) as LayoutInflater
+            val adapter = AlbumSuggestionsAdapter(layoutInflater)
+            //TODO：假数据模拟联想
+            val testList1 = listOf("111", "222", "444", "666")
+            val testList2 = listOf("777", "888", "999", "000")
+            val suggestions1: MutableList<QueryResult> = genSuggestions(testList1)
+            val suggestions2: MutableList<QueryResult> = genSuggestions(testList2)
+            // 设置联想数据
+            adapter.suggestions = suggestions1
+            this.setCustomSuggestionAdapter(adapter)
 
             // 设置监听器
             setOnSearchActionListener(object : MaterialSearchBar.OnSearchActionListener {
@@ -56,7 +66,7 @@ class SearchActivity : BaseActivity<ActivitySearchBinding>(){
                 }
 
                 override fun onSearchConfirmed(text: CharSequence?) {
-                    // 确认搜索，展示搜索结果列表
+                    // 确认搜索，展示搜索结果列表，替换fragment
                     "搜索内容：$text".showToast()
                 }
 
@@ -76,8 +86,14 @@ class SearchActivity : BaseActivity<ActivitySearchBinding>(){
                 }
 
                 override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                    // 空的时候退出搜索会被调用一次，需要屏蔽
+                    if (s?.isBlank() == true) {
+                        return
+                    }
                     // 实时获取联想内容并显示
                     "获取搜索联想：$s".showToast()
+                    adapter.suggestions = suggestions2
+                    adapter.notifyDataSetChanged()
                     // adapter过滤信息
                 }
 
@@ -87,6 +103,16 @@ class SearchActivity : BaseActivity<ActivitySearchBinding>(){
 
             })
         }
+    }
+
+    private fun genSuggestions(testList: List<String>): MutableList<QueryResult> {
+        val suggestions: MutableList<QueryResult> = ArrayList()
+        for (s in testList) {
+            val suggestion = QueryResult()
+            suggestion.keyword = s
+            suggestions.add(suggestion)
+        }
+        return suggestions
     }
 
     private fun MaterialSearchBar.setNavIcon(navIconRes: Int) {
@@ -100,6 +126,9 @@ class SearchActivity : BaseActivity<ActivitySearchBinding>(){
         imageView.setImageResource(navIconRes)
     }
 
+    private fun showSearchSuggestFragment() {
+        mBinding.searchPageContainer
+    }
 
     override fun showProgress(progress: ProgressBean) {
 
