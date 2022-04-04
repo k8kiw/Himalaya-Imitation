217aad344264fdd9b976e66f6546dfe372fb3684
Minami Kotori <15900197156@163.com>
2022-04-04 17:02:19 +0800

    完成订阅表的dao接口

--- common/src/main/java/com/kotori/common/database/AlbumDao.kt
+++ common/src/main/java/com/kotori/common/database/AlbumDao.kt
@@ -9,14 +9,31 @@ interface AlbumDao {
     fun getAlbums(): Flow<List<MyAlbum>>
 
     @Query("SELECT * FROM album WHERE id IN (:albumIds)")
-    fun loadAlbumsByIds(albumIds: List<Long>): Flow<List<MyAlbum>>
+    fun getAlbumsWithFlowByIds(albumIds: List<Long>): Flow<List<MyAlbum>>
+
+    /**
+     * 仅供dao事务使用的查询方法，业务方要使用flow返回的接口
+     */
+    @Query("SELECT * FROM album WHERE id = :albumId")
+    suspend fun getAlbumsById(albumId: Long): List<MyAlbum>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun insertAlbums(vararg myAlbums: MyAlbum)
+    suspend fun insertAlbums(vararg myAlbums: MyAlbum)
 
     @Update
-    fun updateAlbums(vararg myAlbums: MyAlbum)
+    suspend fun updateAlbums(vararg myAlbums: MyAlbum)
 
     @Delete
-    fun deleteAlbums(vararg myAlbums: MyAlbum)
+    suspend fun deleteAlbums(vararg myAlbums: MyAlbum)
+
+    /**
+     * 以事务形式，查询某专辑是否被订阅
+     * @param myAlbum 只考察其id
+     */
+    @Transaction
+    suspend fun isSubscribed(myAlbum: MyAlbum): Boolean {
+        // 得到查询列表
+        val queryList = getAlbumsById(myAlbum.id)
+        return queryList.isNotEmpty()
+    }
 }
