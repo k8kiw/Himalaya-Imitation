f8f6b9bdf93e7fdf99fa7565000a6197b8e70bf8
Minami Kotori <15900197156@163.com>
2022-05-21 11:53:25 +0800

    完成Personal组件的内容显示

--- personal/src/main/java/com/kotori/personal/PersonalFragment.kt
+++ personal/src/main/java/com/kotori/personal/PersonalFragment.kt
@@ -4,6 +4,8 @@ import android.view.View
 import com.kotori.common.base.BaseDbFragment
 import com.kotori.common.entity.ProgressBean
 import com.kotori.common.ui.addLeftCloseImageBtn
+import com.kotori.common.utils.showToast
+import com.kotori.common.widget.SettingView
 import com.kotori.personal.databinding.FragmentPersonalBinding
 
 class PersonalFragment: BaseDbFragment<FragmentPersonalBinding>() {
@@ -12,14 +14,20 @@ class PersonalFragment: BaseDbFragment<FragmentPersonalBinding>() {
 
     override fun getLayoutId(): Int = R.layout.fragment_personal
 
-    override fun showTopBar(): Boolean = true
+    override fun showTopBar(): Boolean = false
 
 
     override fun initView(root: View) {
         getTopBar()?.setTitle(R.string.title_personal)
+        initListener()
+    }
 
-        // 测试返回键
-        //addLeftCloseImageBtn()
+    private fun initListener() {
+        mBinding.settingItemSubscribe.setOnSettingItemClick(object : SettingView.OnSettingItemClick {
+            override fun click(v: View?) {
+                "点击订阅".showToast()
+            }
+        })
     }
 
     override fun showProgress(progress: ProgressBean) {
